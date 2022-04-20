21da515d833cd525c782bfd28dc421827642530f
iknowhtml <iknowsmtp@gmail.com>
2022-04-20 07:40:01 -0400

    Comment out CustomAuth object creation Attempt to minimize minimal reproducible set up

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import CustomAuth from "@toruslabs/customauth";
 
-new CustomAuth({
-  baseUrl: "localhost:3000",
-});
+// new CustomAuth({
+//   baseUrl: "localhost:3000",
+// });
 
 export default function handler(req, res) {
   res.status(200).json({ name: "John Doe" });
