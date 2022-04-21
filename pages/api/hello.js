bc3656da241c0d1e37667d3619d1cc8f762ade22
iknowhtml <iknowsmtp@gmail.com>
2022-04-21 13:10:46 -0400

    Update response

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,4 @@
 // import CustomAuth from "@toruslabs/customauth";
-
-console.log(__dirname);
-
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).send(__dirname);
 }
